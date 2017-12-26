f3b5bec494670fa3288ba3f872603daa7e804c21
Fabio Souza <souzace@gmail.com>
2017-12-26 20:18:05 -0300

    Estrutura Inicial do Projeto

---
+++ app/src/main/java/fsouza/studio/com/githubusermobilesearch/MainActivity.java
@@ -0,0 +1,13 @@
+package fsouza.studio.com.githubusermobilesearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
